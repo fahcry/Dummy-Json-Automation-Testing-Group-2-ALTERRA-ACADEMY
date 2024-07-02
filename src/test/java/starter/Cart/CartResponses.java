9f0f5d08726dcc783cb40adafdfdec5b8785d845
Hary Indra <haryindrapermana1620@gmail.com>
2024-07-02 20:15:02 +0700

    base automationTest DummyJson

---
+++ src/test/java/starter/Cart/CartResponses.java
@@ -0,0 +1,4 @@
+package starter.Cart;
+
+public class CartResponses {
+}
--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "classpath:features",
-        tags = "@tugas"
+        features = "classpath:features"
+//        ,
+//        tags = "@tugas"
 )
 public class CucumberTestSuite {}
