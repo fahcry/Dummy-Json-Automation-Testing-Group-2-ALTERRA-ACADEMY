59dafa5ad15b593cb19242e1c00e6ee20f54e5e7
Samuel Pieter <samuelpieter17@gmail.com>
2024-07-04 21:26:55 +0700

    update

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "classpath:features",
+        features = "classpath:features"
 //        ,
-        tags = "@Tugas"
+//        tags = "@Tugas"
 )
 public class CucumberTestSuite {}
